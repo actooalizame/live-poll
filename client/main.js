f284c7699f3273a48aaa4311733f82694f75c208
gatosaurio <hermeskybalion@hotmail.com>
2015-05-31 06:36:50 -0400

    double login test

--- client/main.js
+++ client/main.js
@@ -9,4 +9,16 @@ accounts-facebook
 accounts-ui
 d0minikk:materialize-meteor
 
+var facebook = "";
+var twitter = "javier en twitter";
+
+function getSocial(){
+  if(facebook===""){
+    return twitter;
+  } else if(twitter===""){
+    return facebook;
+  }
+}
+
+
 */
